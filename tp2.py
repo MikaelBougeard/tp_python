7b29b618b36adad5ef4dd136d0dc6b483f76d6ff
Mikael Bougeard <mikael.bougeard@outlook.fr>
2020-11-24 12:17:54 +0100

    fix + feat
    affichage + fonction

--- tp2.py
+++ tp2.py
@@ -1,21 +1,32 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 
-
 produits = { 1: { 'nom': 'banane', 'prix': 4 }, 2: { 'nom': 'pomme', 'prix': 2 }, 3: { 'nom': 'orange', 'prix': 1.5 }, 4: { 'nom': 'poire', 'prix': 3 } }
+
+def remise(a) :
+    if a>200:
+        b = a*0.95
+        return b
+    else:
+        return a
+
+def tva(a) :
+    return a * 1.20
+
 for key,value in produits.items():
     print('{}\t | {}\t | {}\t '.format(key,value['nom'],value['prix']))
-
     # print(key,"\t",value['nom'],"\t", value['prix'])
+
 totalPrixTTC  = 0
 totalPrixHT = 0
+
 try:
     np = int(input('Combien de produits ? : '))
 except ValueError:
     print("Saisie invalide")
 else:
     for i in range(0, np):
-        print("produit", i+1)
+        print("Commande numéro ", i+1)
         try:
             id_produit = int(input('saisir id produit : '))
             qt=float(input('saisir la quantité : '))
@@ -23,15 +34,17 @@ else:
             print("Id saisie invalide")
         else:
             prixHT=(produits[id_produit]['prix'])*qt
-            prixTTC=prixHT*1.20
+            srPrixHT = prixHT
+            prixHT= remise(prixHT)
+            prixTTC= tva(prixHT)
+
             totalPrixTTC += prixTTC
             totalPrixHT += prixHT
-            remise = 0
-            if totalPrixTTC>200:
-                remise = totalPrixTTC*5/100
-                totalprixTTC=totalPrixTTC*0.95
-            print("Prix HT : " ,prixHT)
-            print("Prix TTC : ", prixTTC)
-            print("Remise : ", remise)
+
+            print("Prix TTC Produit(s) : ", srPrixHT)
+            print("Remise 5% : ", prixHT*5/100)
+            print("Total Prix HT Produit(s) : ", prixHT)
+            print("Total Prix TTC Produit(s) : ", prixTTC)
+
     print("Total Prix HT : " ,totalPrixHT)
     print("Total Prix TTC : ", totalPrixTTC)
